96905a8bb7992d99ca0f5ead08becf9f51fb76e8
masda70@gmail.com <masda70@gmail.com@fafdc67c-6200-3c46-87a9-4bae73d84e93>
2011-11-19 19:29:26 +0000

    working example!

--- webdata/src/main/java/org/moviedb/Main.java
+++ webdata/src/main/java/org/moviedb/Main.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.Charset;
 
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
@@ -16,6 +17,7 @@ import net.sf.saxon.s9api.XsltCompiler;
 import org.htmlcleaner.CleanerProperties;
 import org.htmlcleaner.HtmlCleaner;
 import org.htmlcleaner.PrettyHtmlSerializer;
+import org.htmlcleaner.PrettyXmlSerializer;
 import org.htmlcleaner.TagNode;
 
 public class Main {
@@ -25,28 +27,27 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		try {
-			System.setProperty("http.agent.name", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)");
+
 			// create an instance of HtmlCleaner
 			HtmlCleaner cleaner = new HtmlCleaner();
-			 
-			// take default cleaner properties
+			 // take default cleaner properties
 			CleanerProperties props = cleaner.getProperties();
-			 
-			// customize cleaner's behaviour with property setters
-			 
-			// Clean HTML taken from simple string, file, URL, input stream, 
-			// input source or reader. Result is root node of created 
-			// tree-like structure. Single cleaner instance may be safely used
-			// multiple times.
+
+			props.setPruneTags("script,style");
+			props.setRecognizeUnicodeChars(false);
+
+			props.setAdvancedXmlEscape(false);
+
 			URL url = new URL("http://www.imdb.com/title/tt0068646/");
 			URLConnection conn = url.openConnection();
-			conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB;     rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13 (.NET CLR 3.5.30729)");
+			conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/A.B (KHTML, like Gecko) Chrome/X.Y.Z.W Safari/A.B.");
 	
+			System.out.println(Charset.defaultCharset());
 			TagNode node = cleaner.clean(conn.getInputStream());	
-
-
+			
 			// serialize a node to a file, output stream, DOM, JDom...
-			new PrettyHtmlSerializer(props).writeToFile(node, "C:\\Users\\David Montoya\\Documents\\ProjetWebData\\test2.html");
+			PrettyXmlSerializer serializer= new PrettyXmlSerializer(props);
+			serializer.writeToFile(node, "C:\\Users\\David Montoya\\Documents\\ProjetWebData\\test2.html");
 			
 		       TransformerFactory tFactory = TransformerFactory.newInstance();  
 		        try {  
